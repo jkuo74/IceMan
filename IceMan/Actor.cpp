eced3be01703c3a9c997c618a7df5eea8870eed5
kmz123 <39351882+kmz123@users.noreply.github.com>
2018-05-22 20:06:06 -0700

    Update Actor.cpp

--- IceMan/Actor.cpp
+++ IceMan/Actor.cpp
@@ -10,19 +10,15 @@ int Actor::getState()
 {
 	return state;
 }
-bool Actor::isVisible()
-{
+bool Actor::isVisible() {
 	return visible;
 }
 bool Actor::isAlive()
 {
 	return state != DEAD;
 }
-Person::Person(const int & ID, const int & x_coord, const int & y_coord, const GraphObject::Direction & face, const double & size, const unsigned int & depth, StudentWorld * swp):
-	Actor(ID, x_coord, y_coord, right, size, depth, swp), health_points(10)
-{
-
-}
+Person::Person(const int & ID, const int & x_coord, const int & y_coord, const GraphObject::Direction & face, const double & size, const unsigned int & depth, StudentWorld * swp) :
+	Actor(ID, x_coord, y_coord, right, size, depth, swp), health_points(10) {}
 void Person::annoy()
 {
 	health_points = health_points - 2;
@@ -33,10 +29,8 @@ int Person::getHealth()
 }
 
 
-IceMan::IceMan(StudentWorld * swp):
-	Person(IID_PLAYER, 30, 60, right, 1.0, 0, swp)
-{
-}
+IceMan::IceMan(StudentWorld * swp) :
+	Person(IID_PLAYER, 30, 60, right, 1.0, 0, swp) {}
 void IceMan::doSomething()
 {
 	move();
@@ -56,7 +50,7 @@ void IceMan::move()
 		case KEY_PRESS_LEFT:
 			if (left != dir)
 				setDirection(left);
-			else if(x_pos != 0)
+			else if (x_pos != 0)
 				moveTo(getX() - 1, getY());
 			else
 				moveTo(getX(), getY());
@@ -64,7 +58,7 @@ void IceMan::move()
 		case KEY_PRESS_RIGHT:
 			if (right != dir)
 				setDirection(right);
-			else if(x_pos != 60)
+			else if (x_pos != 60)
 				moveTo(getX() + 1, getY());
 			else
 				moveTo(getX(), getY());
@@ -72,7 +66,7 @@ void IceMan::move()
 		case KEY_PRESS_UP:
 			if (up != dir)
 				setDirection(up);
-			else if(y_pos != 60)
+			else if (y_pos != 60)
 				moveTo(getX(), getY() + 1);
 			else
 				moveTo(getX(), getY());
@@ -80,7 +74,7 @@ void IceMan::move()
 		case KEY_PRESS_DOWN:
 			if (down != dir)
 				setDirection(down);
-			else if(y_pos != 0)
+			else if (y_pos != 0)
 				moveTo(getX(), getY() - 1);
 			else
 				moveTo(getX(), getY());
@@ -94,11 +88,7 @@ Thing::Thing(const int & ID, const int & x_coord, const int & y_coord, const Gra
 	state = PERMANENT;
 }
 
-Ice::Ice(int x_coord, int y_coord, StudentWorld * swp):
-	Thing(IID_ICE, x_coord, y_coord, right, 0.25, 3, swp)
-{
-
-}
+Ice::Ice(int x_coord, int y_coord, StudentWorld * swp) : Thing(IID_ICE, x_coord, y_coord, right, 0.25, 3, swp) {}
 
 Boulder::Boulder(int x_coord, int y_coord, StudentWorld * swp) :
 	Thing(IID_BOULDER, x_coord, y_coord, right, 1.0, 1, swp){}
