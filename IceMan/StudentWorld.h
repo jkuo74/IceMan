e8ead81fae802c5b9a02fdb2e48c87fa1410f807
kmz123 <39351882+kmz123@users.noreply.github.com>
2018-05-22 20:14:49 -0700

    Update StudentWorld.h

--- IceMan/StudentWorld.h
+++ IceMan/StudentWorld.h
@@ -12,31 +12,25 @@
 
 // Students:  Add code to this file, StudentWorld.cpp, Actor.h, and Actor.cpp
 //Pass Student world as static pointer?
+enum ObjType {BOULDER, GOLD, OIL, SONAR, WATER};
 class StudentWorld : public GameWorld
 {
 public:
 	StudentWorld(std::string assetDir);
-
-	bool by_itself(const int & x_coord, const int & y_coord);//checks that no object is placed within 6 blocks apart
-
+	bool by_itself(const int & x_coord, const int & y_coord, const  int & radius);//checks that no object is placed within a radius
 	virtual int init();
-
 	virtual int move();
-
 	//void updateDisplayText();
-
 	void removeIce(const int & x_coord, const int & y_coord);
-
 	bool IceBelow(const int & x_coord, const int & y_coord);
-
 	bool BoulderBelow(const int & x_coord, const int & y_coord);
+	bool makeVisible();
+	void pickUpItem(const int & x_coord, const int & y_coord);
 
 	virtual void cleanUp();
-
 	static StudentWorld* getInstance();
-
+	std::shared_ptr<IceMan> getHero() { return Hero; };
 	~StudentWorld();
-
 private:
 	int current_level_number;
 	static StudentWorld * SWP;
