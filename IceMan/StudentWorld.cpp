a47e1f7ca3a0dc77928307f8eb0f536b3b78cb15
kmz123 <39351882+kmz123@users.noreply.github.com>
2018-05-22 20:13:54 -0700

    Update StudentWorld.cpp

--- IceMan/StudentWorld.cpp
+++ IceMan/StudentWorld.cpp
@@ -2,7 +2,10 @@
 #include <string>
 #include<iostream>
 using namespace std;
+
 StudentWorld * StudentWorld::SWP;
+const static int initTunnelL = 30;
+const static int initTunnelR = 33;
 
 StudentWorld::StudentWorld(string assetDir)
 	: GameWorld(assetDir), current_level_number(0)
@@ -20,67 +23,63 @@ StudentWorld* StudentWorld::getInstance()
 	return SWP;
 }
 
-int StudentWorld::init()
-{
+int StudentWorld::init() {
 	Hero = std::make_shared<IceMan>(this);//add player
-	for (int n = 0; n < 64; n++)
-	{
+	for (int n = 0; n < 64; n++) {
 		std::vector<Ice*> columns;
 		for (int m = 0; m < 64; m++)
-			if (((n < 30 || n > 33) || (m < 4)) && m < 60)
+			if (((n < initTunnelL || n > initTunnelR) || (m < 4)) && m < 60)
 				columns.push_back(new Ice{ n,m });
 			else
 				columns.push_back(nullptr);
 		IceBlocks.push_back(columns);
 	}
 
-	 for (int n = 0; n < 4; n++)
-	 {
-		 std::vector<std::shared_ptr<Actor>> temp_object;
-		 Objects.push_back(temp_object);
-	 }
-	 //Objects[0] = Boulders
-	 //Objects[1] = Gold Nuggets
-	 //Objects[2] = Oil Barrel
-	 //Objects[3] = Sonar Kit
+	for (int n = 0; n < 4; n++) { // creating vector of pointers to each Object type
+		std::vector<std::shared_ptr<Actor>> temp_object;
+		Objects.push_back(temp_object);
+	}
 
-	 int B = std::min((current_level_number / 2) + 2, 9);
-	 for (int n = 0; n < B; n++)
-	 {
-		 int x_rand = (rand() % 27);
-		 int section_rand = (rand() % 2);
-		 if (section_rand == 1)
-			 x_rand = x_rand + 34;
-		 int y_rand = (rand() % 56) + 1;
-		 if (by_itself(x_rand, y_rand))
-		 {
-			 Objects[0].push_back(std::make_shared<Boulder>(x_rand, y_rand, this));
-			 removeIce(x_rand, y_rand);
+	int B = std::min((current_level_number / 2) + 2, 9);
+	int n = 0;
+	while (n <  B) {
+		int x_rand = (rand() % 27);
+		int section_rand = (rand() % 2);
+		if (section_rand == 1)
+			x_rand = x_rand + 34;
+		int y_rand = (rand() % 56) + 1;
+		if (((x_rand < (initTunnelL - 4)) || (x_rand >(initTunnelR + 4))) && by_itself(x_rand, y_rand, 6)) {
+			Objects[BOULDER].push_back(std::make_shared<Boulder>(x_rand, y_rand, this));
+			removeIce(x_rand, y_rand);
+			n++;
+		}
+	}//add boulders
+
+	 int G = std::min((5 - current_level_number / 2), 2); //num of gold  nuggets
+	 n = 0;
+	 while (n < G)  {
+		 int x_rand = (rand() % 60);
+		 int y_rand = (rand() % 56);
+		 if (((x_rand < (initTunnelL - 4)) || (x_rand > (initTunnelR + 4))) && by_itself(x_rand, y_rand, 6)) {
+			 Objects[GOLD].push_back(std::make_shared<Gold_Nugget>(x_rand, y_rand, this));
+			 n++;
+			cerr << "x : " << x_rand << "y : " << y_rand << endl;
 		 }
-	 }//add boulders
-
-	 /*int G = std::min((5 - current_level_number / 2), 2);
-	 for (int n = 0; n < G; n++)
-	 {
-	 int x_rand = (rand() % 60);
-	 int y_rand = (rand() % 56);
-	 if (by_itself(x_rand, y_rand))
-	 Objects[1].push_back(std::make_shared<Gold_Nugget>(x_rand, y_rand));
-	 }//add boulders
-	 */
+	 } // add gold
+	 
 	return GWSTATUS_CONTINUE_GAME;
 }
 
-bool StudentWorld::by_itself(const int & x_coord, const int & y_coord)
+bool StudentWorld::by_itself(const int & x_coord, const int & y_coord, const int & radius) /////////////////////
 {
-	if(Objects.size() != 0)
+	if (Objects.size() != 0)
 		for (auto it = Objects.begin(); it != Objects.end(); it++)
 			for (auto it2 = it->begin(); it2 != it->end(); it2++)
 			{
 				int x_dist = (*it2)->getX() - x_coord;
 				int y_dist = (*it2)->getY() - y_coord;
 				double distance = sqrt(pow(x_dist, 2.0) + pow(y_dist, 2.0));
-				if (distance <= 6.0)
+				if (distance <= radius) /////////////////////
 					return false;
 			}
 	return true;
@@ -108,7 +107,7 @@ bool StudentWorld::IceBelow(const int & x_coord, const int & y_coord)
 
 bool StudentWorld::BoulderBelow(const int & x_coord, const int & y_coord)
 {
-	for (auto it = Objects[0].begin(); it != Objects[0].end(); it++)
+	for (auto it = Objects[BOULDER].begin(); it != Objects[BOULDER].end(); it++)
 	{
 		if ((*it)->isAlive() && abs((*it)->getX() - x_coord) < 4)
 		{
@@ -117,6 +116,12 @@ bool StudentWorld::BoulderBelow(const int & x_coord, const int & y_coord)
 		}
 	}
 	return false;
+}
+bool StudentWorld::makeVisible() {
+	
+}
+void StudentWorld::pickUpItem(const int & x_coord, const int & y_coord) {
+
 }
 
 int StudentWorld::move()
@@ -149,7 +154,7 @@ void StudentWorld::cleanUp() {
 			delete block;
 			block = nullptr;
 		}
-		
+
 	}
 	for (auto & line : Objects) {
 		for (auto & block : line) {
