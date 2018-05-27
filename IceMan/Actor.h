716b2f0f73742c6c2928d318149258512b8a4bd0
kmz123 <39351882+kmz123@users.noreply.github.com>
2018-05-26 22:38:08 -0700

    Update Actor.h

--- IceMan/Actor.h
+++ IceMan/Actor.h
@@ -101,6 +101,12 @@ public:
 	virtual void doSomething();
 	virtual ~Sonar_Kit() {}
 };
+class Water_Pool : public Temp_Thing {
+public:
+	Water_Pool::Water_Pool(const int & x_coord, const int & y_coord, StudentWorld * swp);
+	void doSomething();
+	~Water_Pool() {};
+};
 class Squirt : public Temp_Thing {
 public:
 	Squirt(const int & x_coord, const int & y_coord, const GraphObject::Direction & face, StudentWorld * swp);
